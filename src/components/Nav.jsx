a20f70cb0966d3e2fad6e5022f3450611a957abd
Hardik <hardiknahta111@gmail.com>
2023-12-25 23:48:25 +0530

    updating navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,44 +1,63 @@
-import { hamburger } from "../assets/icons";
-import { headerLogo } from "../assets/images";
-import { navLinks } from "../constants";
-
+import {useState} from 'react'
+import {headerLogo }from '..//assets/images'
+import {hamburger} from '../assets/icons'
+import { navLinks } from '../constants'
 
 const Nav = () => {
-    return (
-      <header className='padding-x py-8 absolute z-10 w-full'>
-        <nav className='flex justify-between items-center max-container'>
-          <a href='/'>
-            <img
-              src={headerLogo}
-              alt='logo'
-              width={129}
-              height={29}
-              className='m-0 w-[129px] h-[29px]'
-            />
-          </a>
-          <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
-            {navLinks.map((item) => (
-              <li key={item.label}>
-                <a
-                  href={item.href}
-                  className='font-montserrat leading-normal text-lg text-slate-gray'
-                >
-                  {item.label}
-                </a>
-              </li>
-            ))}
-          </ul>
-          <div className='flex gap-2 text-lg leading-normal font-medium font-montserrat max-lg:hidden wide:mr-24'>
-            <a href='/'>Sign in</a>
-            <span>/</span>
-            <a href='/'>Explore now</a>
-          </div>
-          <div className='hidden max-lg:block'>
-            <img src={hamburger} alt='hamburger icon' width={25} height={25} />
-          </div>
-        </nav>
-      </header>
-    );
-  };
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const toggleDropdown = () => {
+      setShowDropdown(!showDropdown);
+    };
   
-  export default Nav;
+    const closeDropdown = () => {
+      setShowDropdown(false);
+    };
+  
+  return (
+    <header className='padding-x py-8 absolute z-10 w-full'>
+    <nav className='flex justify-between items-center max-container'>
+        <a href='/'>
+            <img src={headerLogo} alt="Logo" width={130} height={29}  />
+        </a>
+        <ul className='flex-1 flex gap-16 max-lg:hidden justify-center items-center'>
+            {navLinks.map((item) =>(
+                <li key={item.label}>
+                    <a href={item.href}
+                    className='font-montserrat leading-normal text-lg text-slate-400'
+                    >{item.label}</a>
+                </li>
+            ))}
+        </ul>
+        <div className="">
+          <img
+            src={hamburger}
+            alt="Hamburger"
+            width={25}
+            height={25}
+            className='cursor-pointer hidden max-lg:block'
+            onClick={toggleDropdown}
+          />
+          {showDropdown && (
+            <div className="absolute right-0 mt-2 bg-white shadow-md">
+              <ul className='flex flex-col gap-4 px-10 py-3 mt-3 mb-3 w-full'>
+                {navLinks.map((item) => (
+                  <li key={item.label} onClick={closeDropdown}>
+                    <a
+                      href={item.href}
+                      className='font-montserrat leading-normal text-lg text-slate-400'
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+    </nav>
+    </header>
+  )
+}
+
+export default Nav
